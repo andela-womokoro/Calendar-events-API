8a6f3f29b145d29e4a9f1713118feb58af00eb19
Wilson Omokoro <wilson.omokoro@gmail.com>
2021-03-11 23:16:36 +0100

    create controllers

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    /**
+     * Register users
+     * 
+     * @return [type]
+     */
+    public function register()
+    {
+    	//
+    }
+
+    /**
+     * Login users
+     * 
+     * @return [type]
+     */
+    public function login()
+    {
+    	//
+    }
+
+    /**
+     * Logout users
+     * 
+     * @return [type]
+     */
+    public function logout()
+    {
+    	//
+    }
+}
