e961bea1f8022d7e2bd8f25b5791d82ba20c60a3
Wilson Omokoro <wilson.omokoro@gmail.com>
2021-03-11 22:14:18 +0100

    add authentication routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['middleware' => 'api'], function () {
+	Route::post('/register', [UserController::class, 'register']);
+    Route::post('/login', [UserController::class, 'login']);
+});
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+	Route::post('/logout', [UserController::class, 'logout']);
+});
+   
