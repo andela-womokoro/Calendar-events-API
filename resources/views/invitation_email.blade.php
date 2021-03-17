cfb1624c1749fbc5b9b4b147129bcaa486cfb99d
Wilson Omokoro <wilson.omokoro@gmail.com>
2021-03-17 12:58:38 +0100

    add a simplified invitation email template

--- resources/views/invitation_email.blade.php
+++ resources/views/invitation_email.blade.php
@@ -1,1 +1,19 @@
-foo bar
+Hello {{ $details["invitee_name"] }},<br />
+
+<p>You have been invited to the following event:</p>
+
+<p>
+What:	{{ $details["event_description"] }}<br />
+
+When:	{{ $details["event_date"] }}, {{$details["event_time"]}}<br />
+
+Where:	{{ $details["event_location"] }}<br />
+
+Who:	- {{ $details["organizer_name"] }}<br />
+		- {{ $details["invitee_name"] }}
+</p>
+
+<p>Please confirm if you be able to join the meeting.</p>
+<br />
+Best regards,<br />
+{{ $details["organizer_name"] }}
